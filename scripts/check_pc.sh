f63b567d76a2e17cdcfd620b0afa9057a656652f
Carlos Santos <casantosmu@gmail.com>
2026-07-23 17:54:11 +0200

    Add PC heartbeat healthcheck

---
+++ scripts/check_pc.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+HEALTHCHECKS_URL="$(cat "/home/carlos/hp-minipc/secrets/healthchecks_pc_url")"
+/usr/bin/curl -fsS -m 10 --retry 5 -o /dev/null "${HEALTHCHECKS_URL}"
